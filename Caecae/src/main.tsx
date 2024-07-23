60eb0858dc4acf23913fdee9ea61d8dd56152bb2
pccommen <pccommen@gmail.com>
2024-07-23 11:18:07 +0900

    init

---
+++ Caecae/src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-blue-500 text-xl font-bold">Hello, React!</h2>
+      <p className="text-lg font-medium">Hello, Typescript!</p>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
