1b0341e8fe03305745bf9645ed253c4d8d3932cb
cm2hcm2h <cm2hcm2h@naver.com>
2024-07-29 11:52:27 +0900

    design: Footer 컴포넌트 custom css 적용 (CC-89)

--- Caecae/tailwind.config.js
+++ Caecae/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './index.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        footer: '#1C1A1B',
+        footerText: '#888888',
+        footerVerticalLine: '#444444',
+      }
+    },
   },
   plugins: [],
 }
-
