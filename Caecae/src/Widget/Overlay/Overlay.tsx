a3a350b9a8f71443d987c72a36fefb763dee4555
pccommen <pccommen@gmail.com>
2024-07-30 17:58:34 +0900

    fix: overlay에 있던 에러 수정 (CC-106)

--- Caecae/src/Widget/Overlay/Overlay.tsx
+++ Caecae/src/Widget/Overlay/Overlay.tsx
@@ -1,19 +1,15 @@
 import React, { ReactNode } from "react";
-import useWork from "../../../Shared/Hyundux/Hooks/useWork";
-import {
-  initOverlayState,
-  overlayReducer,
-  action,
-} from "../../../Job/Overlay/OverlayWork";
-import store from "../../../Shared/Hyundux/Store";
-import findChildrenElement from "../../../Shared/Util/FindChildrenElement";
+import { initOverlayState, action } from "../../Job/Overlay/OverlayWork";
+import store from "../../Shared/Hyundux/Store";
+import findChildrenElement from "../../Shared/Util/FindChildrenElement";
+import useExistState from "../../Shared/Hyundux/Hooks/useExistState";
 
 interface OverLayProps {
   children: ReactNode;
 }
 
 const OverLay: React.FC<OverLayProps> = ({ children }) => {
-  const state = useWork(initOverlayState, overlayReducer);
+  const state = useExistState(initOverlayState);
   const content = findChildrenElement(
     children,
     (element) =>
@@ -22,7 +18,8 @@ const OverLay: React.FC<OverLayProps> = ({ children }) => {
 
   if (state.isShowing) {
     return (
-      <div className="absolute inset-0 bg-black opacity-50 flex flex-col items-center justify-center">
+      <div className="absolute z-[100] inset-0 flex flex-col items-center justify-center">
+        <div className="absolute bg-black opacity-50 w-full h-full"></div>
         <div
           className="relative bg-white shadow-lg z-10"
           style={{ width: "62%", height: "71%" }}
