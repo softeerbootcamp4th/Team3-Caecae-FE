48e46b0ca64c3b55c71b969fdc07c830c3b6d2a0
pccommen <pccommen@gmail.com>
2024-07-31 10:22:32 +0900

    fix: 사용하지않는 useRef삭제 (CC-108)

--- Caecae/src/Page/FindingGame/Enter/EnterContent.tsx
+++ Caecae/src/Page/FindingGame/Enter/EnterContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import SmileBadge from "../../../Widget/SmileBadge/SmileBadge";
 
 interface EnterContentProps {
