45db41e7442ff4462877d78a453d7295383cd1cc
minani-0621 <cm2hcm2h@naver.com>
2024-08-08 17:39:27 +0900

    feat: ShowCasper1 section 구현 (CC-122)

---
+++ Caecae/src/Page/EventInfoLanding/Sections/ShowCasper1.tsx
@@ -0,0 +1,32 @@
+const ShowCasper1 = () => {
+    return (
+        <>
+            <div className="flex w-full h-screen justify-center relative">
+                <div className="flex flex-col absolute z-20 items-center top-48">
+                    <p className="text-[28px] text-[#F7F7F7]">
+                        CASPER Electric
+                        <span className="text-[28px] text-[#CCCCCC] pl-2">신차 출시 이벤트</span>
+                    </p>
+                    <p className="text-[36px] text-white mt-2">
+                        캐스퍼 일렉트릭
+                        <span className="text-[36px] text-[#CCCCCC]">과 함께하는 7일간의 캐미</span>
+                    </p>
+                    <p className="text-[36px] text-white">
+                        07.18.&nbsp;&nbsp;⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯&nbsp;&nbsp;07.25.
+                    </p>
+                    <h1 className="text-[96px] text-white mt-10 font-bold">
+                        "너의 능력을 보여줘"
+                    </h1>
+                </div>
+                <div>
+                    <img 
+                        src="/src/Shared/assets/eventInfoPage1.svg"
+                        alt="eventInfoPage1"
+                        className="absolute top-0 left-0 w-full h-screen object-cover object-bottom-center" />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ShowCasper1;
