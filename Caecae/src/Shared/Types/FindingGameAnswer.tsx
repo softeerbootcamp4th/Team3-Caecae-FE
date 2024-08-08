8d92bbfd2b6ef0edf0836188590af56aeaa12888
pccommen <pccommen@gmail.com>
2024-08-08 17:15:05 +0900

    fix: 게임의 정답 타입 수정 (CC-128)

--- Caecae/src/Shared/Types/FindingGameAnswer.tsx
+++ Caecae/src/Shared/Types/FindingGameAnswer.tsx
@@ -2,8 +2,9 @@ interface FindingGameAnswer {
   id: number;
   y: number;
   x: number;
-  imageURL: string | null;
-  info: string | null;
+  imageURL: string;
+  title: string;
+  info: string;
 }
 
 export default FindingGameAnswer;
