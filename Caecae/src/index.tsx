1d300c937cd75d97172b7fee3d48399b44105a3c
pccommen <pccommen@gmail.com>
2024-08-13 18:07:04 +0900

    fix: import path 추가 (CC-137)

--- Caecae/src/index.tsx
+++ Caecae/src/index.tsx
@@ -6,6 +6,7 @@ import FindingGameLandingPage from "./pages/FindingGameLanding/FindingGameLandin
 import RacingGameLandingPage from "./pages/RacingGameLanding/RacingGameLandingPage.tsx";
 import FindingGamePage from "./pages/FindingGame/FindingGamePage.tsx";
 import RacingGamePage from "./pages/RacingGame/RacingGamePage.tsx";
+import AdminPage from "./pages/Admin/AdminPage.tsx";
 
 // 임시 React component
 const App = () => {
