a602c66ac7a458a348356d0631106cc95287aae0
minani-0621 <cm2hcm2h@naver.com>
2024-08-15 15:58:21 +0900

    feat: 레이싱 게임 랜딩페이지에 레이싱 게임 시작 가능 여부 api 연결 (CC-158)

--- Caecae/src/pages/RacingGameLanding/RacingGameLandingPage.tsx
+++ Caecae/src/pages/RacingGameLanding/RacingGameLandingPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Navigation from "../../components/common/Navigation";
 import {
   EventIntro, 
@@ -7,19 +8,35 @@ import {
   EventPrecaution,
  } from "../../features/RacingGameLanding";
 import Footer from "../../components/common/Footer";
+import getRacingGameAvailable from "../../stories/getRacingGameAvailable";
 
 const RacingGameLandingPage = () => {
-    return (
-        <>
-          <Navigation />
-          <EventIntro />
-          <HowToEvent />
-          <GiftInfo />
-          <EventPeriod />
-          <EventPrecaution />
-          <Footer />
-        </>
-    );
+  const [isEventOpen, setIsEventOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getRacingGameAvailable();
+      const racingGameOpen = response.data;
+
+      if(racingGameOpen === true) {
+        setIsEventOpen(true);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  return (
+      <>
+        <Navigation />
+        <EventIntro />
+        <HowToEvent />
+        <GiftInfo />
+        <EventPeriod />
+        <EventPrecaution />
+        <Footer />
+      </>
+  );
 };
 
 export default RacingGameLandingPage;
