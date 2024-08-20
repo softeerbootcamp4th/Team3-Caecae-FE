553e6b624ef4bc90d5827241145f95cc239c8e48
minani-0621 <cm2hcm2h@naver.com>
2024-08-20 11:12:24 +0900

    refactor: 레이싱 게임에 useRacingGameAudio 커스텀 훅 적용 (CC-169)

--- Caecae/src/components/RacingGame/RacingGame.tsx
+++ Caecae/src/components/RacingGame/RacingGame.tsx
@@ -11,8 +11,222 @@ import {
 import { store, useExistState } from "../../shared/Hyundux/index.tsx";
 import Link from "../../shared/Hyunouter/Link.tsx";
 import getRacingGameTopRate from "../../stories/getRacingGameTopRate.tsx";
-import { resetAudio, playAudio, fadeOutAudio } from "../../utils/audioManipulate.tsx";
 import { useDebounce } from "../../hooks/index.tsx";
+import useRacingGameAudio from "../../hooks/useRacingGameAudio.tsx";
+
+const RacingGame: React.FC = () => {
+  const lottieRef = useRef<LottieRefCurrentProps | null>(null);
+  const frontRef = useRef<HTMLDivElement>(null);
+  const rearRef = useRef<HTMLDivElement>(null);
+  const [frontBackgroundWidth, setFrontImageWidth] = useState<number>(0);
+  const [rearBackgroundWidth, setRearBackgroundWidth] = useState<number>(0);
+  const state = useExistState(initRacingGameState);
+  const [topRate, setTopRate] = useState<string | null>(null);
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+  const debouncedDistance = useDebounce(state.distance, 50);
+
+  /** 모션 값을 사용하여 frontBackground의 x 위치 추적 */
+  const frontX = useMotionValue(0);
+
+  /** 애니메이션 제어를 위한 framer-motion 훅 */
+  const frontAnimationControls = useAnimation();
+  const rearAnimationControls = useAnimation();
+
+  const endGameTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const {
+    startPlayingAudio,
+    startStoppingAudio,
+    fadeOutPlayingAudio,
+    resetAllAudio,
+    stoppingAudioRunRef,
+  } = useRacingGameAudio(
+    "/assets/audio/racingGamePlayingSound.wav",
+    "/assets/audio/racingGameStopSound.wav"
+  );
+
+  /** 이동한 km를 구하는 함수 */
+  const calculateDistance = (x: number) => {
+    const totalDistance = Math.abs(x);
+
+    store.dispatch(action.updateDistance(totalDistance));
+  };
+
+  const handleSmoothlyStop = () => {
+    const moveMoreDistance = 500;
+
+    if (endGameTimeoutRef.current) {
+      clearTimeout(endGameTimeoutRef.current);
+    }
+
+    if (lottieRef.current) {
+      lottieRef.current?.pause();
+
+      /** 현재 위치 가져오기 (getBoundingClientRect 사용) */
+      const currentFrontX = frontRef.current?.getBoundingClientRect().x || 0;
+      const currentRearX = rearRef.current?.getBoundingClientRect().x || 0;
+
+      frontAnimationControls.start({
+        x: currentFrontX - moveMoreDistance,
+        transition: { duration: 1, ease: "easeOut" },
+      });
+
+      rearAnimationControls.start({
+        x: currentRearX - moveMoreDistance,
+        transition: { duration: 1, ease: "easeOut" },
+      });
+
+      fadeOutPlayingAudio(1000, startStoppingAudio);
+    }
+  };
+
+  const handleSpacebar = (event: KeyboardEvent) => {
+    if (event.code === "Space") {
+      event.preventDefault();
+
+      handleSmoothlyStop();
+      store.dispatch(action.gameEnd());
+    }
+  };
+
+  const handlePlayGame = () => {
+    setIsButtonDisabled(true);
+    setTopRate("?");
+
+    stoppingAudioRunRef.current = false;
+    startPlayingAudio();
+
+    store.dispatch(action.gameStart());
+
+    if (lottieRef.current) {
+      lottieRef.current?.play();
+
+      frontAnimationControls
+        .start({
+          x: [0, -14000],
+          transition: { duration: 7, repeat: 0 },
+        })
+        .then(() => {
+          handleSmoothlyStop();
+          store.dispatch(action.gameEnd());
+        });
+
+      rearAnimationControls.start({
+        x: [0, -7000],
+        transition: { duration: 7, repeat: 0 },
+      });
+
+      endGameTimeoutRef.current = setTimeout(() => {
+        handleSmoothlyStop();
+        store.dispatch(action.gameEnd());
+      }, 6000);
+    }
+  };
+
+  const enterEvent = () => {
+    store.dispatch(action.enterEvent());
+  }
+
+  useEffect(() => {
+    const frontBackgroundImg = new Image();
+    frontBackgroundImg.src = frontBackground;
+    frontBackgroundImg.onload = () => {
+      setFrontImageWidth(frontBackgroundImg.width);
+    };
+
+    const rearBackgroundImg = new Image();
+    rearBackgroundImg.src = rearBackground;
+    rearBackgroundImg.onload = () => {
+      setRearBackgroundWidth(rearBackgroundImg.width);
+    };
+
+    return () => {
+      resetAllAudio();
+      if(endGameTimeoutRef.current) {
+        clearTimeout(endGameTimeoutRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (state.gameStatus === "playing") {
+      document.addEventListener("keydown", handleSpacebar);
+    } else {
+      document.removeEventListener("keydown", handleSpacebar);
+    }
+    return () => {
+      document.removeEventListener("keydown", handleSpacebar);
+    };
+  }, [state.gameStatus]);
+
+  /** 애니메이션의 움직인 거리(x좌표값)가 바뀔 때 마다 km를 계산 */
+  useEffect(() => {
+    const unsubscribeFrontX = frontX.onChange((latest) => {
+      calculateDistance(latest);
+    });
+
+    return () => unsubscribeFrontX();
+  }, [frontX]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getRacingGameTopRate(debouncedDistance);
+        setTopRate(response.data.percent.toFixed(3));
+      } catch (error) {
+        console.error("레이싱 게임 점수 백분위 API 호출 오류:", error);
+        setTopRate(null);
+      }
+    };
+
+    if(debouncedDistance > 0 && state.gameStatus === "end"){
+      fetchData();
+    }
+  }, [debouncedDistance]);
+
+  useEffect(() => {
+    if (state.gameStatus === "end") {
+      const timer = setTimeout(() => {
+        setIsButtonDisabled(false);
+      }, 2000);
+  
+      return () => clearTimeout(timer);
+    }
+  }, [state.gameStatus]);
+
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      <motion.div
+        ref={rearRef}
+        className="absolute top-0 left-0 h-[700px] bg-[auto_100%] z-[1]"
+        style={{
+          width: `${rearBackgroundWidth}px`,
+          backgroundImage: `url(${rearBackground})`,
+        }}
+        animate={rearAnimationControls}
+      />
+      <motion.div
+        ref={frontRef}
+        className="absolute top-0 left-0 h-[700px] bg-[auto_100%] z-[2]"
+        style={{
+          width: `${frontBackgroundWidth}px`,
+          backgroundImage: `url(${frontBackground})`,
+          x: frontX,
+        }}
+        animate={frontAnimationControls}
+      />
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={animationGame315}
+        loop={true}
+        autoplay={false}
+        className="absolute top-[485px] left-[250px] w-[350px] h-auto z-[3]"
+      />
+      {gameContent(state.gameStatus, state.distance, topRate, isButtonDisabled, handlePlayGame, enterEvent)}
+      {gameMenu(state.gameStatus)}
+    </div>
+  );
+};
 
 /** 게임 상태에 따라 다르게 보여지는 콘텐츠 */
 const gameContent = (
@@ -124,223 +338,6 @@ const gameMenu = (gameStatus: string) => {
     default:
       return null;
   }
-};
-
-const RacingGame: React.FC = () => {
-  const lottieRef = useRef<LottieRefCurrentProps | null>(null);
-  const frontRef = useRef<HTMLDivElement>(null);
-  const rearRef = useRef<HTMLDivElement>(null);
-  const [frontBackgroundWidth, setFrontImageWidth] = useState<number>(0);
-  const [rearBackgroundWidth, setRearBackgroundWidth] = useState<number>(0);
-  const state = useExistState(initRacingGameState);
-  const [topRate, setTopRate] = useState<string | null>(null);
-  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
-  const debouncedDistance = useDebounce(state.distance, 50);
-
-  /** 모션 값을 사용하여 frontBackground의 x 위치 추적 */
-  const frontX = useMotionValue(0);
-
-  /** 애니메이션 제어를 위한 framer-motion 훅 */
-  const frontAnimationControls = useAnimation();
-  const rearAnimationControls = useAnimation();
-
-  const playingSoundRef = useRef<HTMLAudioElement | null>(null);
-  const stopSoundRef = useRef<HTMLAudioElement | null>(null);
-  const stopSoundPlayedRef = useRef<boolean>(false);
-  const endGameTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  /** 이동한 km를 구하는 함수 */
-  const calculateDistance = (x: number) => {
-    const totalDistance = Math.abs(x);
-
-    store.dispatch(action.updateDistance(totalDistance));
-  };
-
-  const handleSmoothlyStop = () => {
-    const moveMoreDistance = 500;
-
-    if (endGameTimeoutRef.current) {
-      clearTimeout(endGameTimeoutRef.current);
-    }
-
-    if (lottieRef.current) {
-      lottieRef.current?.pause();
-
-      /** 현재 위치 가져오기 (getBoundingClientRect 사용) */
-      const currentFrontX = frontRef.current?.getBoundingClientRect().x || 0;
-      const currentRearX = rearRef.current?.getBoundingClientRect().x || 0;
-
-      frontAnimationControls.start({
-        x: currentFrontX - moveMoreDistance,
-        transition: { duration: 1, ease: "easeOut" },
-      });
-
-      rearAnimationControls.start({
-        x: currentRearX - moveMoreDistance,
-        transition: { duration: 1, ease: "easeOut" },
-      });
-
-      fadeOutAudio(playingSoundRef.current, 1000, () => {
-        if (!stopSoundPlayedRef.current) {
-          stopSoundPlayedRef.current = true;
-          playAudio(stopSoundRef.current);
-        }
-      });
-    }
-  };
-
-  const handleSpacebar = (event: KeyboardEvent) => {
-    if (event.code === "Space") {
-      event.preventDefault();
-
-      handleSmoothlyStop();
-      store.dispatch(action.gameEnd());
-    }
-  };
-
-  const handlePlayGame = () => {
-    setIsButtonDisabled(true);
-    setTopRate("?");
-
-    stopSoundPlayedRef.current = false;
-    playAudio(playingSoundRef.current);
-
-    store.dispatch(action.gameStart());
-
-    if (lottieRef.current) {
-      lottieRef.current?.play();
-
-      frontAnimationControls
-        .start({
-          x: [0, -14000],
-          transition: { duration: 7, repeat: 0 },
-        })
-        .then(() => {
-          handleSmoothlyStop();
-          store.dispatch(action.gameEnd());
-        });
-
-      rearAnimationControls.start({
-        x: [0, -7000],
-        transition: { duration: 7, repeat: 0 },
-      });
-
-      endGameTimeoutRef.current = setTimeout(() => {
-        handleSmoothlyStop();
-        store.dispatch(action.gameEnd());
-      }, 6000);
-    }
-  };
-
-
-
-  const enterEvent = () => {
-    store.dispatch(action.enterEvent());
-  }
-
-  useEffect(() => {
-    const frontBackgroundImg = new Image();
-    frontBackgroundImg.src = frontBackground;
-    frontBackgroundImg.onload = () => {
-      setFrontImageWidth(frontBackgroundImg.width);
-    };
-
-    const rearBackgroundImg = new Image();
-    rearBackgroundImg.src = rearBackground;
-    rearBackgroundImg.onload = () => {
-      setRearBackgroundWidth(rearBackgroundImg.width);
-    };
-
-    playingSoundRef.current = new Audio("/assets/audio/racingGamePlayingSound.wav");
-    stopSoundRef.current = new Audio("/assets/audio/racingGameStopSound.wav");
-
-    return () => {
-      resetAudio(playingSoundRef.current);
-      resetAudio(stopSoundRef.current);
-      if (endGameTimeoutRef.current) {
-        clearTimeout(endGameTimeoutRef.current);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (state.gameStatus === "playing") {
-      document.addEventListener("keydown", handleSpacebar);
-    } else {
-      document.removeEventListener("keydown", handleSpacebar);
-    }
-    return () => {
-      document.removeEventListener("keydown", handleSpacebar);
-    };
-  }, [state.gameStatus]);
-
-  /** 애니메이션의 움직인 거리(x좌표값)가 바뀔 때 마다 km를 계산 */
-  useEffect(() => {
-    const unsubscribeFrontX = frontX.onChange((latest) => {
-      calculateDistance(latest);
-    });
-
-    return () => unsubscribeFrontX();
-  }, [frontX]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getRacingGameTopRate(debouncedDistance);
-        setTopRate(response.data.percent.toFixed(3));
-      } catch (error) {
-        console.error("레이싱 게임 점수 백분위 API 호출 오류:", error);
-        setTopRate(null);
-      }
-    };
-
-    if(debouncedDistance > 0 && state.gameStatus === "end"){
-      fetchData();
-    }
-  }, [debouncedDistance]);
-
-  useEffect(() => {
-    if (state.gameStatus === "end") {
-      const timer = setTimeout(() => {
-        setIsButtonDisabled(false);
-      }, 2000);
-  
-      return () => clearTimeout(timer);
-    }
-  }, [state.gameStatus]);
-
-  return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <motion.div
-        ref={rearRef}
-        className="absolute top-0 left-0 h-[700px] bg-[auto_100%] z-[1]"
-        style={{
-          width: `${rearBackgroundWidth}px`,
-          backgroundImage: `url(${rearBackground})`,
-        }}
-        animate={rearAnimationControls}
-      />
-      <motion.div
-        ref={frontRef}
-        className="absolute top-0 left-0 h-[700px] bg-[auto_100%] z-[2]"
-        style={{
-          width: `${frontBackgroundWidth}px`,
-          backgroundImage: `url(${frontBackground})`,
-          x: frontX,
-        }}
-        animate={frontAnimationControls}
-      />
-      <Lottie
-        lottieRef={lottieRef}
-        animationData={animationGame315}
-        loop={true}
-        autoplay={false}
-        className="absolute top-[485px] left-[250px] w-[350px] h-auto z-[3]"
-      />
-      {gameContent(state.gameStatus, state.distance, topRate, isButtonDisabled, handlePlayGame, enterEvent)}
-      {gameMenu(state.gameStatus)}
-    </div>
-  );
 };
 
 export default RacingGame;
