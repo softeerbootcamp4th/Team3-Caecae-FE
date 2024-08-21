ddef1af3b701ba93d14fe4669e2a9f20b2556001
pccommen <pccommen@gmail.com>
2024-08-21 17:51:20 +0900

    fix: getRacingGameOption 변경 (CC-167)

--- Caecae/src/stories/getRacingGameOption.tsx
+++ Caecae/src/stories/getRacingGameOption.tsx
@@ -1,23 +1,21 @@
-import { Story } from "../shared/Hyundux-saga";
-import huynxios from "../shared/Hyunxios"
+import huynxios from "../shared/Hyunxios";
 import Response from "../utils/Response";
 
 export interface GetRacingGameOptionBodyParameter {
-    phone: string;
-    selection: number;
+  phone: string;
+  selection: number;
 }
 
 export interface getRacingGameOptionDTO {
-    phone: string;
-    selection: number;
+  phone: string;
+  selection: number;
 }
 
-const getRacingGameOption: Story = async (request: object) => {
-    const optionObject = request as { phone: string, selection: number }
-    const response = await huynxios.post<Response<getRacingGameOptionDTO>>(
-        "/api/racing/option",
-        request
-    );
-    return response;
+const setRacingGameOption = async (phone: string, selection: number) => {
+  const response = await huynxios.post<Response<getRacingGameOptionDTO>>(
+    "/api/racing/option",
+    { phone: phone, selection: selection }
+  );
+  return response;
 };
-export { getRacingGameOption };
+export { setRacingGameOption };
