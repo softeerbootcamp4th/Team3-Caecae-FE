7351e7832216006b9c9a1fd75f1778590e3e9643
pccommen <pccommen@gmail.com>
2024-08-23 17:55:17 +0900

    [Test] FindingGame Click Action test (CC-178)

--- Caecae/__tests__/FindGameTest/FindGameTest.ts
+++ Caecae/__tests__/FindGameTest/FindGameTest.ts
@@ -1,4 +1,3 @@
-import { act } from "react";
 import {
   initFindingGameState,
   action,
@@ -9,7 +8,7 @@ import createHynduxTester from "../../src/shared/Hyundux-test/Tester";
 
 const tester = createHynduxTester(initFindingGameState, findingGameReducer);
 
-test("changShowingAnswer__action_index_1__result_1_true", () => {
+test("결과보여지는 것들의 인덱스변경_입력값:1 -> 결과값 1 (옮은 테스트)", () => {
   const givenData = initFindingGameState.payload;
   const whenData: Action = action.changeShowingAnswer(1);
   const thenData = tester.makeThenPayload({ answerIndex: 1 })
@@ -17,7 +16,7 @@ test("changShowingAnswer__action_index_1__result_1_true", () => {
   tester.given(givenData).when(whenData).then(thenData, true);
 });
 
-test("changShowingAnswer__action_index_2__result_1_false", () => {
+test("결과보여지는 것들의 인덱스변경_입력값:2 -> 결과값 1 (잘못된 테스트)", () => {
   const givenData = initFindingGameState.payload;
   const whenData: Action = action.changeShowingAnswer(2);
   const thenData = tester.makeThenPayload({ answerIndex: 1 })
@@ -25,3 +24,124 @@ test("changShowingAnswer__action_index_2__result_1_false", () => {
   tester.given(givenData).when(whenData).then(thenData, false);
 });
 
+test("클릭 이벤트:입력값 [{x: 0.5 y: 0.5}, 틀린답 {x: 0.7, y: 0.1}] -> 결과값 answers.length == 1, answers = [answer{ x: 0.5. y: 0.5 }] (옳은 테스트)", () => {
+  const givenData = initFindingGameState.payload;
+  const whenData: Action = action.click({ request: {
+    answerList: [
+      { positionX: 0.5, positionY: 0.5 }
+    ]
+  }, response: {
+      responseCode: 1000,
+      message: "success",
+      data: {
+        ticketIryd: "1212",
+        startTime: "asdsad",
+        correctAnswerList: [
+          {
+            positionX: 0.5,
+            positionY: 0.5,
+            descriptionImageUrl: "",
+            title: "",
+            content: ""
+          }
+        ]
+      }
+  }})
+  
+  const thenData = tester.makeThenPayload({ showingAnswers: [{
+    positionX: 0.5,
+    positionY: 0.5,
+    descriptionImageUrl: "",
+    title: "",
+    content: ""
+  }] })
+  
+  tester.given(givenData).when(whenData).then(thenData, true);
+});
+
+test("클릭 이벤트:입력값 [{x: 0.5 y: 0.5}] -> 결과값 answers.length == 2, answers = [answer{ x: 0.5. y: 0.5 }] (옳은 테스트)", () => {
+  const givenData = initFindingGameState.payload;
+  const whenData: Action = action.click({ request: {
+    answerList: [
+      { positionX: 0.5, positionY: 0.5 },
+    ]
+  }, response: {
+      responseCode: 1000,
+      message: "success",
+      data: {
+        ticketIryd: "1212",
+        startTime: "asdsad",
+        correctAnswerList: [
+          {
+            positionX: 0.5,
+            positionY: 0.5,
+            descriptionImageUrl: "",
+            title: "",
+            content: ""
+          }
+        ]
+      }
+  }})
+  
+  const thenData = tester.makeThenPayload({ showingAnswers: [{
+    positionX: 0.5,
+    positionY: 0.5,
+    descriptionImageUrl: "",
+    title: "",
+    content: ""
+  }]})
+  
+  tester.given(givenData).when(whenData).then(thenData, true);
+});
+
+test("클릭 이벤트:입력값 [{x: 0.5 y: 0.5}, 맞은답 {x: 0.7, y: 0.1}] -> 결과값 answers.length == 2, answers = [answer{ x: 0.5. y: 0.5 }, {x: 0.7, y: 0.1}] (잘못된 테스트)", () => {
+  console.log("gameStatuse가 바뀌어야함")
+  console.log("tickerID도 바뀌어야함 바뀌어야함")
+  const givenData = initFindingGameState.payload;
+  const whenData: Action = action.click({ request: {
+    answerList: [
+      { positionX: 0.5, positionY: 0.5 },
+      { positionX: 0.7, positionY: 0.1 },
+    ]
+  }, response: {
+      responseCode: 1000,
+      message: "success",
+      data: {
+        ticketIryd: "1212",
+        startTime: "asdsad",
+        correctAnswerList: [
+          {
+            positionX: 0.5,
+            positionY: 0.5,
+            descriptionImageUrl: "",
+            title: "",
+            content: ""
+          },
+          {
+            positionX: 0.7,
+            positionY: 0.1,
+            descriptionImageUrl: "",
+            title: "",
+            content: ""
+          }
+        ]
+      }
+  }})
+  
+  const thenData = tester.makeThenPayload({ showingAnswers: [{
+    positionX: 0.5,
+    positionY: 0.5,
+    descriptionImageUrl: "",
+    title: "",
+    content: ""
+  },
+  {
+    positionX: 0.7,
+    positionY: 0.1,
+    descriptionImageUrl: "",
+    title: "",
+    content: ""
+  }]})
+  
+  tester.given(givenData).when(whenData).then(thenData, false);
+});
