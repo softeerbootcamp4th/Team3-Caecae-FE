3f00bfc56401964b49b8fe81877e877784c4249a
pccommen <pccommen@gmail.com>
2024-08-23 11:11:30 +0900

    chore: 테스트를 위한 package json 설정 및 프로젝트 설정 (CC-178)

---
+++ Caecae/jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts?(x)'],
+};
