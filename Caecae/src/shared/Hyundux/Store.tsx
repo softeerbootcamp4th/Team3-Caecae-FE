644ccc8421d05aae9afdb0f832dea9ba8e533530
pccommen <pccommen@gmail.com>
2024-08-23 11:37:38 +0900

    fix: Doaction 삭제 (CC-178)

--- Caecae/src/shared/Hyundux/Store.tsx
+++ Caecae/src/shared/Hyundux/Store.tsx
@@ -1,4 +1,4 @@
-import { Action, DoAction } from "./Actions";
+import { Action } from "./Actions";
 import State from "./State";
 import Reducer from "./Reducer";
 import removeFirst from "./Util/RemoveFirst";
@@ -13,7 +13,7 @@ interface Store {
     reducer: Reducer<PayLoad> | null,
     cb: (state: State<PayLoad>) => void
   ) => void;
-  dispatch: <T>(action: Action | DoAction<T>) => void;
+  dispatch: <T>(action: Action) => void;
   publish: <PayLoad>(oldState: State<PayLoad>, state: State<PayLoad>) => void;
   subscribeList: Map<string, <PayLoad>(state: State<PayLoad>) => void>;
 }
@@ -24,33 +24,22 @@ export function createStore(): Store {
     reducers: [],
     subscribeList: new Map(),
     dispatch: async function (action) {
-      if (isAction(action)) {
-        const reducer = this.reducers.filter(
-          (reducer) => reducer.type == action.type
-        )[0].reducer;
+      const reducer = this.reducers.filter(
+        (reducer) => reducer.type == action.type
+      )[0].reducer;
 
-        const { removed, newArray } = removeFirst(
-          this.states,
-          // eslint-disable-next-line
-          // @ts-ignore
-          // eslint-disable-next-line
-          (state) => (state as State<any>).type == action.type
-        );
-        const newState = await reducer(removed, action);
+      const { removed, newArray } = removeFirst(
+        this.states,
+        // eslint-disable-next-line
+        // @ts-ignore
+        // eslint-disable-next-line
+        (state) => (state as State<any>).type == action.type
+      );
+      const newState = await reducer(removed, action);
 
-        // 여기서 모든것을 바로 state를 적용하는것이 아니라 이게 다른 state도 propagation하는지도 확인해야함
-        this.states = [...newArray, newState];
-        this.publish(removed, newState);
-      }
-      // else if (isDoAction(action)) {
-      //   const { removed, newArray } = removeFirst(
-      //     this.states,
-      //     (state) => state.type == action.type
-      //   );
-      //   const newState = action.doing(removed);
-      //   this.states = [...newArray, newState];
-      //   this.publish(removed, newState);
-      // }
+      // 여기서 모든것을 바로 state를 적용하는것이 아니라 이게 다른 state도 propagation하는지도 확인해야함
+      this.states = [...newArray, newState];
+      this.publish(removed, newState);
     },
     publish: function (oldState, state) {
       const publishedCallBack = this.subscribeList.get(state.type);
@@ -81,14 +70,6 @@ export function createStore(): Store {
 }
 
 const store = createStore();
-
-function isAction(action: unknown): action is Action {
-  return (action as Action).actionName !== undefined;
-}
-
-function isDoAction<T>(action: unknown): action is DoAction<T> {
-  return (action as DoAction<T>).doing !== undefined;
-}
 
 export type { Store };
 export default store;
